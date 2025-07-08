d530f924c5d6ee3dcf2a96ad4ae0360d4bc8e60d
raydon <auk7495@gmail.com>
2025-07-08 18:08:38 +0100

    Update ready.js

--- Events/ready.js
+++ Events/ready.js
@@ -1,6 +1,18 @@
 module.exports = {
   name: 'ready',
   async execute(bot) {
-    await bot.user.setPresence({ activities: [{ name: 'by ray.', type: 5 }], status: 'idle' });
+    await bot.user.setPresence({
+      activities: [{
+        name: 'by Ray',
+        type: 5, // 'PLAYING' activity type
+        buttons: [
+          { label: 'Add Bot', url: 'https://discord.com/oauth2/authorize?client_id=1391415226192105564&permissions=964220542016&integration_type=0&scope=bot' },
+          { label: 'Join Game', url: 'https://discord.com/oauth2/authorize?client_id=1391415226192105564&permissions=964220542016&integration_type=0&scope=bot' }
+        ]
+      }],
+      status: 'idle'
+    });
+
+    console.log(`${bot.user.tag} is online and presence set!`);
   },
 };
